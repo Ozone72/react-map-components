9112a4cfc2ff52f11927c61a2aa97f912bd13812
Orin Fletcher <orin.fletcher@gmail.com>
2020-12-29 14:03:30 -0800

    removes hardcoded Cards and props.  Replaces with map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import Card from "./Card";
 import contacts from "../contacts";
 
 function createCard(contact) {
-  return 
+  return (
   <Card
     key={contact.id}
     name={contact.name}
     img={contact.imgURL}
     tel={contact.phone}
     email={contact.email}
-  />
+  />)
 }
 
 function App() {
@@ -18,24 +18,6 @@ function App() {
     <div>
       <h1 className="heading">My Contacts</h1>
       {contacts.map(createCard)}
-      {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
     </div>
   );
 }
